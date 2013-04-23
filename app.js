76a6273ce6a3201c5f841c69162eaded5632c912
Xiongfei(Alex) Guo <xfguo@credosemi.com>
2013-04-23 15:15:08 +0800

    MIME support.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var http = require('http');
 var url = require("url");
 var fs = require("fs");
 var path = require("path");
+var mime = require("./mime").types;
+
 var server = http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
     var realPath = STATIC_DIR + pathname;
@@ -20,7 +22,10 @@ var server = http.createServer(function(request, response) {
                     response.writeHead(500, {'Content-Type': 'text/plain'});
                     response.end(err);
                 } else {
-                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    var ext = path.extname(realPath);
+                    ext = ext ? ext.slice(1) : 'unknown';
+                    var contentType = mime[ext] || "text/plain";
+                    response.writeHead(200, {'Content-Type': contentType});
                     response.write(file, "binary");
                     response.end();
                 }
